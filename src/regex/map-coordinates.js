4b8035ebdeb6b3f4d96c5cb0b4b642dacb083dab
Marius Kaufmann <marius.kaufmann@googlemail.com>
2024-10-06 23:42:55 +0200

    document MapCoordinates and explain the named groups returned by the regex

--- src/regex/map-coordinates.js
+++ src/regex/map-coordinates.js
@@ -3,6 +3,13 @@
 import {pattern} from 'regex';
 import Expression from './expression';
 
+/**
+ * Parses a map coordinate pair. Currently only supports main map coordinates,  
+ * Example: `123|456`  
+ * Returns the following named groups when matching:  
+ * `x`: x coordinate  
+ * `y`: y coordinate
+ */
 class MapCoordinates extends Expression {
   static regexPattern = pattern`
     (?<x> \d+)\|(?<y> \d+)
