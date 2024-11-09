abca0716b1b32cd7f70bc0d4ed1bcfb7405cb444
Marius Kaufmann <marius.kaufmann@googlemail.com>
2024-11-09 22:43:46 +0100

    fix accidentally overwriting the wrong method in last commit

--- src/line-type/generic-type.js
+++ src/line-type/generic-type.js
@@ -1,9 +1,9 @@
 "use strict";
 
-import LineTag from "./tags/line-tag.js";
+import LineTag from "./tags/line-tag";
 
 class GenericType {
-  static _regexByLanguage = {}
+  static _regexByLanguage = {};
   /**
    * checks if log line matches this line type
    * @param {string} text text to be detected
@@ -11,8 +11,11 @@ class GenericType {
    * @returns true if given log line matches with this log type, otherwise false
    */
   static detect(text, language) {
-    // always return false as no line should be detected as generic type
-    return false;
+    if (typeof this._regexByLanguage[language] === "undefined") {
+      // language not supported for this type
+      return false;
+    }
+    return text.match(this._regexByLanguage[language]) !== null;
   }
 
   /**
@@ -21,12 +24,9 @@ class GenericType {
    * @param {string} language language that the log line is in
    * @returns object parsed object
    */
-  static detect(text, language) {
-    if (typeof this._regexByLanguage[language] === "undefined") {
-      // language not supported for this type
-      return false;
-    }
-    return text.match(this._regexByLanguage[language]) !== null;
+  static parse(text, language) {
+    // nothing to return
+    return {}
   }
 
   /**
