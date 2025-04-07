50466ef062afff07655850b72c153c617c16d3b8
Marius Kaufmann <marius.kaufmann@googlemail.com>
2025-04-07 02:10:43 +0200

    Add missing BeamItemResult parse result

---
+++ src/regex/parse-result/beam-item-result.js
@@ -0,0 +1,19 @@
+"use strict"
+
+import ItemResult from "./item-result.js";
+
+class BeamItemResult {
+  /**
+   * item that was transfered
+   * @type {ItemResult}
+   */
+  item;
+
+  /**
+   * number of items that were transfered
+   * @type {number}
+   */
+  amount;
+}
+
+export default BeamItemResult;
