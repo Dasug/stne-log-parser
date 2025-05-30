1653d5e80c559b494d39913be1daa3cafd9f57dc
Marius Kaufmann <marius.kaufmann@googlemail.com>
2025-05-30 12:16:16 +0200

    Implement ship registration in AvatarEmergencyShieldActivationFailureType statistics population

--- tests/line-type/avatar-emergency-shield-activation-failure-type.test.js
+++ tests/line-type/avatar-emergency-shield-activation-failure-type.test.js
@@ -2,6 +2,7 @@ import { describe, expect, test } from '@jest/globals';
 import LineTag from '../../src/enum/line-tag.js';
 import AvatarJob from '../../src/enum/avatar-job.js';
 import AvatarEmergencyShieldActivationFailureType from '../../src/line-type/avatar-emergency-shield-activation-failure-type.js';
+import Statistics from '../../src/statistics/statistics.js';
 
 describe('avatar emergency shield activation failure line type', () => {
   const lineTypeClass = AvatarEmergencyShieldActivationFailureType;
@@ -36,5 +37,19 @@ describe('avatar emergency shield activation failure line type', () => {
     expect(parseResult.ship.nccPrefix).toBeNull();
     expect(parseResult.ship.shipClass).toBe("Terran Excelsior Refit");
     
+  });
+
+  test("registers ships in statistics", () => {
+    const statistics = new Statistics;
+    const testLogEntry = { "lang": "de", "entry": String.raw`Patrick Greene (1093078, Verteidigungstaktiker) an Bord von =MS= Aquilon Kratos (2481039, Terran Excelsior Refit) bemerkt die drohnende Gefahr zu spät und schafft es nicht rechtzeitig die Schilde zu aktivieren!` };
+    const parseResult = lineTypeClass.parse(testLogEntry.entry, testLogEntry.lang);
+
+    lineTypeClass.populateStatistics(statistics, parseResult);
+
+    expect(statistics.ships.mentionedShips.length).toBe(1);
+    const ship = statistics.ships.getShipByNcc(2481039);
+    expect(ship).not.toBeNull();
+    expect(ship.ncc).toBe(2481039);
+    expect(ship.name).toBe("=MS= Aquilon Kratos");
   });
 })
