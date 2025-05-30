adebfbfb66e7068d616d4d5d401e8bb2faae6e5b
Marius Kaufmann <marius.kaufmann@googlemail.com>
2025-05-30 12:18:30 +0200

    Implement ship registration in AvatarEmergencyShieldActivationType statistics population

--- tests/line-type/avatar-emergency-shield-activation-type.test.js
+++ tests/line-type/avatar-emergency-shield-activation-type.test.js
@@ -2,6 +2,7 @@ import { describe, expect, test } from '@jest/globals';
 import LineTag from '../../src/enum/line-tag.js';
 import AvatarJob from '../../src/enum/avatar-job.js';
 import AvatarEmergencyShieldActivationType from '../../src/line-type/avatar-emergency-shield-activation-type.js';
+import Statistics from '../../src/statistics/statistics.js';
 
 describe('avatar emergency shield actvation line type', () => {
   const lineTypeClass = AvatarEmergencyShieldActivationType;
@@ -36,5 +37,19 @@ describe('avatar emergency shield actvation line type', () => {
     expect(parseResult.ship.nccPrefix).toBeNull();
     expect(parseResult.ship.shipClass).toBe("Terran Excelsior Refit");
     
+  });
+
+  test("registers ships in statistics", () => {
+    const statistics = new Statistics;
+    const testLogEntry = { "lang": "de", "entry": String.raw`Maria Weiss (1124429, Verteidigungstaktiker) an Bord von =MS= Euros Deimos (2558180, Terran Excelsior Refit) reagiert blitzschnell und schafft es in buchstäblich letzter Sekunde den Schildauslöser zu erreichen!` };
+    const parseResult = lineTypeClass.parse(testLogEntry.entry, testLogEntry.lang);
+
+    lineTypeClass.populateStatistics(statistics, parseResult);
+
+    expect(statistics.ships.mentionedShips.length).toBe(1);
+    const ship = statistics.ships.getShipByNcc(2558180);
+    expect(ship).not.toBeNull();
+    expect(ship.ncc).toBe(2558180);
+    expect(ship.name).toBe("=MS= Euros Deimos");
   });
 })
