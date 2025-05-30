db8b4ce7310025dc3a06067073d20f5178afeca0
Marius Kaufmann <marius.kaufmann@googlemail.com>
2025-05-30 22:43:46 +0200

    Export statistics class

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import StneLogParser from "./stne-log-parser.js";
 import LogEntry from "./log-entry.js";
 import LogLine from "./log-line.js";
 import { lineTypes, lineTypesByName } from "./line-types.js";
+import Statistics from "./statistics/statistics.js";
 
-export { StneLogParser as default, StneLogParser, LogEntry, LogLine, lineTypes, lineTypesByName };
+export { StneLogParser as default, StneLogParser, LogEntry, LogLine, lineTypes, lineTypesByName, Statistics };
