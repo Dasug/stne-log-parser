c80393cac43cf87ea81c0b9aef10e84c15e8d7c0
Marius Kaufmann <marius.kaufmann@googlemail.com>
2025-05-30 22:07:26 +0200

    Implement ship registration in TractorBeamLockType statistics population

--- src/line-type/tractor-beam-lock-type.js
+++ src/line-type/tractor-beam-lock-type.js
@@ -8,6 +8,7 @@ import { pattern } from "regex";
 import LineTag from "../../src/enum/line-tag.js";
 import MapCoordinates from "../regex/subroutine/map-coordinates.js";
 import TractorBeamResult from "./parse-result/tractor-beam-result.js";
+import Statistics from '../statistics/statistics.js';
 
 class TractorBeamLockType extends GenericType {
   static _regexByLanguage = {
@@ -41,6 +42,18 @@ class TractorBeamLockType extends GenericType {
     resultObject.sector = sector;
 
     return resultObject;
+  }
+
+  /**
+   * @inheritdoc
+   * @override
+   */
+  static populateStatistics(/** @type {Statistics}*/ statistics, parseResult) {
+    // register ships
+    statistics.ships.registerShip(parseResult.ship);
+    statistics.ships.registerShip(parseResult.target);
+    
+    return statistics;
   }
 
   static getTags() {
--- tests/line-type/tractor-beam-lock-type.test.js
+++ tests/line-type/tractor-beam-lock-type.test.js
@@ -2,6 +2,7 @@ import { describe, expect, test } from '@jest/globals';
 import LineTag from '../../src/enum/line-tag.js';
 import ShipNameOnlyResult from '../../src/regex/parse-result/ship-name-only-result.js';
 import TractorBeamLockType from '../../src/line-type/tractor-beam-lock-type.js';
+import Statistics from '../../src/statistics/statistics.js';
 
 describe('tractor beam lock line type', () => {
   const lineTypeClass = TractorBeamLockType;
@@ -38,5 +39,23 @@ describe('tractor beam lock line type', () => {
     // sector
     expect(parseResult.sector.x).toBe(141);
     expect(parseResult.sector.y).toBe(526);
+  });
+
+  test("registers ships in statistics", () => {
+    const statistics = new Statistics;
+    const testLogEntry = { "lang": "de", "entry": String.raw`=MS= Hivemother Hexara erfasst in Sektor(141|526) die [Scout] Deudi mit einem Traktorstrahl.` };
+    const parseResult = lineTypeClass.parse(testLogEntry.entry, testLogEntry.lang);
+
+    lineTypeClass.populateStatistics(statistics, parseResult);
+
+    expect(statistics.ships.mentionedShips.length).toBe(2);
+    const ship = statistics.ships.getShipByName("=MS= Hivemother Hexara");
+    expect(ship).not.toBeNull();
+    expect(ship.ncc).toBeNull();
+    expect(ship.name).toBe("=MS= Hivemother Hexara");
+    const ship2 = statistics.ships.getShipByName("[Scout] Deudi");
+    expect(ship2).not.toBeNull();
+    expect(ship2.ncc).toBeNull();
+    expect(ship2.name).toBe("[Scout] Deudi");
   });
 })
