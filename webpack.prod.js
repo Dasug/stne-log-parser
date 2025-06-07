5224feb52d2e97f5005002e23aa5ab5d6d076170
Marius Kaufmann <marius.kaufmann@googlemail.com>
2025-06-07 22:15:41 +0200

    Remove externals declarations for external libraries
    It makes it somewhat complicated and they're pretty small anyways so I'll bundle them for now.

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,17 +4,5 @@ import common from './webpack.common.js';
 export default merge(common, {
   mode: 'production',
   externals: {
-    enumify: {
-      commonjs: 'enumify',
-      commonjs2: 'enumify',
-      amd: 'enumify',
-      root: 'Enumify',
-    },
-    regex: {
-      commonjs:'regex',
-      commonjs2: 'regex',
-      amd: 'regex',
-      root: 'Regex',
-    }
   },
 });
