6c45547cb0fbb3176f833e51a296c551370a1060
Marius Kaufmann <dasug@improved-madness.de>
2025-11-22 15:08:11 +0100

    Add log line parsing and color highlighting to demonstration page

--- src/demonstration/section/log-parser.js
+++ src/demonstration/section/log-parser.js
@@ -1,5 +1,6 @@
 "use strict"
 
+import LogEntry from '../../log-entry.js';
 import LogHeadParser from '../../regex/log-head-parser.js';
 
 function setupLogParser() {
@@ -7,13 +8,55 @@ function setupLogParser() {
   input.addEventListener("input", () => handleInput(input, input.innerText));
 }
 
+function processLogEntry(logEntryInputField, workQueue) {
+  if(workQueue.length === 0) {
+    return;
+  }
+  const work = workQueue.pop();
+  const entry = work.entry;
+  const entryDiv = work.entryDiv;
+  let parsedEntry = null;
+  try {
+     parsedEntry = LogEntry.parseLogMessage(entry);
+  } catch(e) {
+    console.error("could not parse log entry", {entry, e});
+  }
+
+  
+  if(parsedEntry !== null) {
+    logEntryInputField.parsedEntries.push(parsedEntry);
+    const parsedLines = parsedEntry.parsedLines;
+    const logLineDivs = Array.from(entryDiv.querySelectorAll(".log-line:not(.log-header)"));
+    for (let index = 0; index < logLineDivs.length; index++) {
+      const lineDiv = logLineDivs[index];
+      const parsedLine = parsedLines[index] ?? null;
+      if(parsedLine === null) {
+        console.error("parsed line and line divs are not synchronized somehow...", {parsedLines, logLineDivs});
+        return;
+      }
+      lineDiv.classList.remove("log-line-pending");
+      if(parsedLine.detected) {
+        lineDiv.classList.add("log-line-parsed");
+      } else {
+        lineDiv.classList.add("log-line-not-parsed");
+      }
+    }
+  }
+
+  // go to the next item when done.
+  window.setTimeout(() => processLogEntry(logEntryInputField, workQueue), 100);
+}
+
 function handleInput(inputField, inputContent) {
+  // prepare log entry dataset
+  inputField.parsedEntries = [];
   const lines = inputContent.split("\n");
   const logEntries = LogHeadParser.parseMessages(inputContent);
   const logElements = [];
   let currentEntry = null;
   let currentEntryDiv = null;
   const lastEntryLine = logEntries.reduce((acc, entry) => Math.max(acc, entry.lineEnd), 0);
+  const entryDivs = [];
 
   for (let lineNum = 0; lineNum < lines.length; lineNum++) {
     const line = lines[lineNum];
@@ -21,11 +64,12 @@ function handleInput(inputField, inputContent) {
       currentEntry = null;
       currentEntryDiv = null;
     } else {
-      logEntries.forEach(entry => {
+      logEntries.forEach((entry, idx) => {
         if(lineNum >= entry.lineStart && lineNum <= entry.lineEnd && currentEntry !== entry) {
           currentEntry = entry;
           currentEntryDiv = document.createElement("div");
           currentEntryDiv.classList.add("log-entry-boundary");
+          entryDivs[idx] = currentEntryDiv;
           logElements.push(currentEntryDiv);
         }
       });
@@ -46,6 +90,16 @@ function handleInput(inputField, inputContent) {
     }
   }
   inputField.replaceChildren(...logElements)
+
+  const workQueue = [...logEntries].map((entry, idx) => {
+    return {
+      entry: entry,
+      entryDiv: entryDivs[idx],
+    };
+  }).sort((a,b) => (a.entry.dateTime - b.entry.dateTime));
+  window.setTimeout(() => {
+    processLogEntry(inputField, workQueue);
+  }, 100);
 }
 
 export {setupLogParser};
